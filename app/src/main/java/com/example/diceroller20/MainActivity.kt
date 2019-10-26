84ea76c8e190b9f4cae77b6e7844652da429f4db
Gaabu <56568395+Gaabu@users.noreply.github.com>
2019-10-26 11:22:37 +0800

    Revert "rollDice With Image"

--- app/src/main/java/com/example/diceroller20/MainActivity.kt
+++ app/src/main/java/com/example/diceroller20/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.diceroller20
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import org.w3c.dom.Text
-import java.util.*
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,36 +15,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-
+        val rollButton2: Button = findViewById(R.id.roll_button2)
 
         rollButton.setOnClickListener { rollDice() }
-
+        rollButton2.setOnClickListener {countUP()}
 
     }
 
 
     private fun rollDice(){
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val resultText: TextView = findViewById(R.id.result_text)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView  = findViewById(R.id.result_text3)
+        val rnds = (1..6).random()
+        val rnds2 = (1..6).random()
+        val rnds3 = (1..6).random()
 
-        val randomInt = Random().nextInt(6)+1
-
-
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-        diceImage.setImageResource(drawableResource)
-
+        resultText.text = rnds.toString()
+        resultText2.text = rnds2.toString()
+        resultText3.text = rnds3.toString()
 
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
     }
 
+    private fun countUP(){
+        val showResult : TextView = findViewById(R.id.result_text)
+        val showResult2: TextView = findViewById(R.id.show_result)
 
+        val result1 = "Hello Friend!"
+        val num1 = 1
+        val parseInt = Integer.parseInt(showResult.text.toString()) + 1
+
+
+        if(showResult.text == result1){
+            showResult2.text = num1.toString()
+        }else if(showResult.text == "6"){
+            showResult2.text = "6"
+        }else {
+            showResult2.text = parseInt.toString()
+        }
+    }
 }
